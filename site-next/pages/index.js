88a977c794af2f0b4a51cd105395b053b14d5d12
Germano Lira <dolan.3@hotmail.com>
2021-05-09 19:25:37 -0300

    estruturando o projeto

---
+++ site-next/pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Synthesis</title>
+        <meta name="description" content="Synthesis site" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        
+      </main>
+
+      <footer className={styles.footer}>
+        
+        </a>
+      </footer>
+    </div>
+  )
+}
